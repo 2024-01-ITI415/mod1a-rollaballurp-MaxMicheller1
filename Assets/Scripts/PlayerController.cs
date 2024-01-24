5c5c4df5df11126d10dd356c549888d9d9393a3f
MaxMicheller1 <157072693+MaxMicheller1@users.noreply.github.com>
2024-01-24 00:24:46 -0500

    Displaying Score and Text (Finished)
    Revise the PlayerController script to store the value of collected PickUp GameObjects
    Create and configure UI Text Elements to display:
    The count value
    An end of game message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb; 
+	private int count;
     private float movementX;
     private float movementY;
     public float speed = 0; 
-
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
     
     // Start is called before the first frame update
     void Start()
     {
-    rb = GetComponent <Rigidbody>();       
+    rb = GetComponent <Rigidbody>(); 
+	count = 0; 
+	SetCountText();
+	winTextObject.SetActive(false);
     }
 
     void OnMove (InputValue movementValue)
@@ -26,8 +31,17 @@ public class PlayerController : MonoBehaviour
 	movementX = movementVector.x; 
 	movementY = movementVector.y; 
 	}
+	
+	void SetCountText() 
+	{
+	countText.text =  "Count: " + count.ToString();
+	if (count >= 20)
+		{
+		winTextObject.SetActive(true);
+		}
+	}
 
-   private void FixedUpdate() 
+	private void FixedUpdate() 
 	{
 	Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 	rb.AddForce(movement * speed); 
@@ -38,6 +52,8 @@ public class PlayerController : MonoBehaviour
 	if (other.gameObject.CompareTag("PickUp")) 
 		{
 			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
 		}
 	}
 }
